d41f363a0ea96e779e2c8cbbf575b56b9582bfef
Gabriel Felipe Vieira <ogabriel.v.felipe@gmail.com>
2024-08-15 11:40:04 -0300

    fundamentals 1 -  Part 2

--- src/server.js
+++ src/server.js
@@ -1,10 +1,9 @@
 import http from 'node:http';
 import { json } from './middlewares/json.js';
-import { Database } from './database.js';
-import { randomInt, randomUUID } from 'node:crypto';
+import { routes } from './routes.js';
+import { extractQueryParams } from './util/extract-query-params.js';
 
 
-const database = new Database()
 
     
 
@@ -13,25 +12,20 @@ const server = http.createServer(async(req, res) => {
 
     await json(req, res)
 
-    if (method === 'GET' && url === '/users'){
-        const users = database.select('users')
-        return res.end(JSON.stringify(users))
-    }
+    const route = routes.find( route => {
+        return route.method === method && route.path.test(url)
+    })
 
-    if (method === 'POST' && url === '/users'){
+
+    if (route){
+        const routeParams = req.url.match(route.path)
+       
+        const {query, ...params} = routeParams.groups
+
+        req.params = params
+        req.query = query ? extractQueryParams(query) : {}
         
-        const { name, email } = req.body;
-
-        
-        const user = {
-            id: randomUUID(), 
-            name,
-            email
-        }
-
-        database.insert('users', user)
-
-        return res.writeHead(201).end()
+        return route.handler(req, res)
     }
 
     return res.writeHead(404).end('')
